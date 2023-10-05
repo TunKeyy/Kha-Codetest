dd7e1eb6a94687abd8cfd2e05b77501f87b5b049
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-10-05 14:37:53 +0700

    commit new version

--- frontend/src/Components/Cart/Cart.js
+++ frontend/src/Components/Cart/Cart.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import nike from "../assets/nike.png"
+import poker from "../assets/poker-cards.png"
 import trash from "../assets/trash.png"
+import edit from "../assets/edit.png"
+import save from "../assets/save.png"
+import gold from "../assets/medal-gold.png"
+import silver from "../assets/medal-silver.png"
+import bronze from "../assets/medal-bronze.png"
 
 import "./Cart.css"
 import { useCart } from "react-use-cart";
@@ -24,15 +29,15 @@ const Cart = () => {
     <div className='container-border'>
             <div className='App-card'>
                 <div className='App-card-Top'>
-                    <img src={nike}></img>
+                    <img src={poker}></img>
                 </div>
                 
-                <div className="App-card-Title">Your Cart
-                <span className="App-card-TitleAmount">$0.00</span>
+                <div className="App-card-Title">Table of scores
+                <span className="App-card-TitleAmount"></span>
                 </div>
                 <div className="App-card-Body">
                     <div className="App-card-empty">
-                        <p>Your Cart is empty</p>
+                        <p>Your table is empty</p>
                     </div>
                 </div>
             </div>
@@ -42,11 +47,11 @@ const Cart = () => {
         <div className='container-border'>
             <div className='App-card'>
                 <div className='App-card-Top'>
-                    <img src={nike}></img>
+                    <img src={poker} ></img>
                 </div>
                 
-                <div className="App-card-Title">Your Cart
-                <span className="App-card-TitleAmount">${cartTotal}</span>
+                <div className="App-card-Title">Table of scores
+                <span className="App-card-TitleAmount"></span>
                 </div>
                 <div></div>
                 <div className="App-card-Body">
@@ -56,27 +61,49 @@ const Cart = () => {
                         <div className='App-cart-Item'>
                             <div className='App-cart-Item-Left'>
                                 <div className='Cart-img' style={{backgroundColor: item.color}}>
-                                    <img src={item.image} alt={item.name} />
+                                    {/* <img src={item.image} alt={item.name} /> */}
                                 </div>
                             </div>
                             <div className='App-cart-Item-Right'>
                                 <div className='Cart-name'>
                                     {item.name}
+                                    <button onClick={() => removeItem(item.id)} className='Cart-item-remove'>
+                                        <img src={trash}/>
+                                    </button>
+                                    <button onClick={() => {
+                                        document.getElementById(item.id).style.display = "block";
+                                        document.getElementById("Cart-item-save" + item.id).style.display = "block";
+                                        document.getElementById(item.id).value = item.quantity;
+                                    }} className='Cart-item-edit'>
+                                        <img src={edit}/> </button>
                                 </div>
                                 <div className='Cart-price'>
-                                    ${item.price}
+                                    Score: {item.quantity - 1} 
+                                    <div className='edit-wrapper'>
+                                     <input id={item.id} type='number'/>
+                                     <button id={"Cart-item-save" + item.id} onClick={() => {
+                                         updateItemQuantity(item.id, document.getElementById(item.id).value);
+                                         document.getElementById(item.id).style.display = "none";
+                                        document.getElementById("Cart-item-save" + item.id).style.display = "none";
+                                     }} className='Cart-item-save'>
+                                        <img src={save}/>
+                                        </button>
+                                    </div>
                                 </div>
                                 <div className='Cart-actions'>
                                     <div className='Cart-item-count'>
                                         <button className='Cart-item-count-button'
-                                        onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>-</button>
-                                        <div className='Cart-item-count-number'>{item.quantity}</div>
+                                        onClick={() => updateItemQuantity(item.id, Number(item.quantity) + 1)}><img src={bronze}/></button>
                                         <button className='Cart-item-count-button'
-                                        onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
+                                        onClick={() => updateItemQuantity(item.id, Number(item.quantity) + 2)}><img src={silver}/></button>
+                                        <button className='Cart-item-count-button'
+                                        onClick={() => updateItemQuantity(item.id, Number(item.quantity) + 3)}><img src={gold}/></button>
+                                        <button className='Cart-item-count-button'
+                                        onClick={() => updateItemQuantity(item.id, Number(item.quantity) - 1)}>-1</button>
+                                        <button className='Cart-item-count-button'
+                                        onClick={() => updateItemQuantity(item.id, Number(item.quantity) + 1)}>+1</button>
                                     </div>
-                                    <button onClick={() => removeItem(item.id)} className='Cart-item-remove'>
-                                        <img src={trash}/>
-                                    </button>
+                                    
                                 </div>
                             </div>
                         </div>
--- frontend/src/Components/Products/Product.js
+++ frontend/src/Components/Products/Product.js
@@ -1,40 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import data from "../../data/shoes.json"
-import nike from "../assets/nike.png"
+import poker from "../assets/poker-cards.png"
 import check from "../assets/check.png"
+import edit from "../assets/edit.png"
+import save from "../assets/save.png"
 
 import './Product.css'
 import { useCart } from "react-use-cart";
 
 const Product = () => {
-    const { addItem, items } = useCart();
+    const { addItem, items, updateItem } = useCart();
     
     return (
         <div className='container-border'>
             <div className='App-card'>
                 <div className='App-card-Top'>
-                    <img src={nike} ></img>
+                    <img src={poker} ></img>
                 </div>
                 
-                <div className="App-card-Title">Our Products</div>
+                <div className="App-card-Title">Members</div>
                 <div className="App-card-Body">
                     {data.shoes.map((item) => {
                         const inCart = items.find((cartItem) => cartItem.id === item.id);
                         return (
                     <div className='App-card-Item'>
-                        <div className='Item-img' style={{backgroundColor: item.color}}>
+                        {/* <div className='Item-img' style={{backgroundColor: item.color}}>
                             <img src={item.image}  alt={item.name} />
-                        </div>
+                        </div> */}
                         <div className='Item-name'>
-                            {item.name}
+                            <span id={'item-name'+ item.id}>{item.name}</span>
+                            <input id={'item-new-name'+ item.id} type='text'/>
+                            <button id={'Cart-item-edit' + item.id} onClick={() => {
+                                        document.getElementById('item-name'+ item.id).style.display = "none";
+                                        document.getElementById('item-new-name'+ item.id).style.display = "inline-block";
+                                        document.getElementById('Cart-item-edit' + item.id).style.display = "none";
+                                        document.getElementById('Cart-item-save-left' + item.id).style.display = "inline-block";
+                                    }} className='Cart-item-edit'>
+                            <img src={edit}/> </button>
+
+                            <button id={'Cart-item-save-left' + item.id} onClick={() => {
+                                updateItem(item.id, {name: String(document.getElementById('item-new-name'+ item.id).value)});
+                                document.getElementById('item-new-name'+ item.id).style.display = "none";
+                                document.getElementById('Cart-item-edit' + item.id).style.display = "inline-block";
+                                document.getElementById('item-name'+ item.id).style.display = "inline-block";
+                                document.getElementById('Cart-item-save-left' + item.id).style.display = "none";
+                                document.getElementById('item-name'+ item.id).textContent = item.name;
+                                }} className='Cart-item-save-left'>
+                            <img src={save}/>
+                            </button>
                         </div>
                         <div className='Item-description'>
-                            {item.description}
+                            {/* {item.description} */}
                         </div>
                         <div className='Item-bottom'>
                             <div className='Item-price'>
-                            ${item.price}
+                            {/* ${item.price} */}
                             </div>
                             {inCart ? (<div className='Item-check'>
                                 <img src={check} />
@@ -42,7 +63,7 @@ const Product = () => {
                             <button onClick={() => 
                                 addItem(item)
                                 } className='Item-button'>
-                                    ADD TO CART
+                                    ADD TO ROOM
                             </button>
                             )
                             }
